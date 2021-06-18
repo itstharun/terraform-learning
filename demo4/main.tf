efa43c3cedec48f90bc9dbca9315cd8e59f7d9ad
itstharun <55631302+itstharun@users.noreply.github.com>
2021-06-18 16:03:55 +0530

    Update main.tf

--- demo4/main.tf
+++ demo4/main.tf
@@ -14,8 +14,8 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket"
+resource "awss3buckettk" "b" {
+  bucket = "tk-test-bucket"
   acl    = "private"
 
   tags = {
