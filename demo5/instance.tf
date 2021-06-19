015252f2accc5f85345250a04ccb5354049ddd33
itstharun <55631302+itstharun@users.noreply.github.com>
2021-06-19 10:11:13 +0530

    Create instance.tf

---
+++ demo5/instance.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "example" {
+  ami           = var.AMIS[var.AWS_REGION]
+  instance_type = "t2.micro"
+
+  subnet_id = aws_subnet.tk-public-1.id
+
+  #security group
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+
+  #the public SSH key
+  key_name = aws_key_pair.mykeypair.key_name
+}
