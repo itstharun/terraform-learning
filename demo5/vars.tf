d05eae7ac91787ca13081392fc1bbbe7f3e0f29d
itstharun <55631302+itstharun@users.noreply.github.com>
2021-06-19 10:56:43 +0530

    Update vars.tf

--- demo5/vars.tf
+++ demo5/vars.tf
@@ -14,8 +14,8 @@ variable "PATH_TO_PUBLIC_KEY" {
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-0d204b1f624e3b51d"
-    us-west-2 = "ami-0dd273d94ed0540c0"
-    us-west-1 = "ami-0daf1443f6adffe0d"
+    us-east-1 = "ami-2865093e"
+    us-west-2 = "ami-800c94e0"
+    us-west-1 = "ami-c8ba9da8"
   }
 }
